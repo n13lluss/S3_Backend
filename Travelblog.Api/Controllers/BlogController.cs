748378566eea322352bc7b7a81468b8aa3ef7ad8
niels van lierop <nielsvanlierop15@gmail.com>
2024-02-26 13:20:15 +0100

    got the blog api running with all requests

---
+++ Travelblog.Api/Controllers/BlogController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Travelblog.Core.Interfaces;
+using Travelblog.Core.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Travelblog.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        public List<Blog> blogs = new List<Blog>();
+        // GET: api/<BlogController>
+        [HttpGet]
+        [Route("getAll")]
+        public IEnumerable<Blog> Get()
+        {
+            return blogs.ToArray();
+        }
+
+        // GET api/<BlogController>/5
+        [HttpGet("getById={id}")]
+        public Blog Get(int id)
+        {
+            return blogs[id];
+        }
+
+        // POST api/<BlogController>
+        [HttpPost("create")]
+        public IActionResult Create([FromBody] Blog newBlog)
+        {
+            if (newBlog == null)
+            {
+                return BadRequest("Invalid input");
+            }
+            newBlog.Id = blogs.Count;
+            newBlog.StartDate = DateTime.UtcNow;
+            blogs.Add(newBlog);
+            return CreatedAtAction(nameof(Get), new { id = newBlog.Id }, newBlog);
+        }
+
+        // PUT api/<BlogController>/5
+        [HttpPut("update={id}")]
+        public IActionResult Put(int id, [FromBody] Blog updatedBlog)
+        {
+            if (id < 0 || id >= blogs.Count)
+            {
+                return NotFound();
+            }
+            if (updatedBlog == null)
+            {
+                return BadRequest("Invalid input");
+            }
+            blogs[id] = updatedBlog;
+            return NoContent();
+        }
+
+        // DELETE api/<BlogController>/5
+        [HttpDelete("delete={id}")]
+        public IActionResult Delete(int id)
+        {
+            if (id < 0 || id >= blogs.Count)
+            {
+                return NotFound();
+            }
+            blogs.RemoveAt(id);
+            return NoContent();
+        }
+    }
+}
--- Travelblog.Core/Models/Blog.cs
+++ Travelblog.Core/Models/Blog.cs
@@ -11,14 +11,14 @@ namespace Travelblog.Core.Models
         public int Id { get; set; }
         public int User_Id { get; set; }
         public string User_Name { get; set; }
-        public int Likes { get; set; }
+        public int Likes { get; set; } = 0;
         public List<Post> Posts { get; set; } = [];
         public DateTime StartDate { get; set; }
-        public bool IsPrive { get; set; }
-        public bool IsSuspended { get; set; }
-        public bool IsDeleted { get; set; }
+        public bool IsPrive { get; set; } = false;
+        public bool IsSuspended { get; set; } = false;
+        public bool IsDeleted { get; set; } = false;
         public List<User> Followers { get; set; } = [];
-        public int Trip_Id { get; set; }
-        public List<Country> Countries { get; set; }
+        public int Trip_Id { get; set; } = 0;
+        public List<Country> Countries { get; set; } = new List<Country>();
     }
 }
