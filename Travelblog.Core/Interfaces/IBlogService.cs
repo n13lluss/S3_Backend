37c3391e243e2cc4f98f679455f733522c9e536b
n13lluss <nielsvanlierop15@gmail.com>
2024-02-29 14:14:40 +0100

    updated blogservice

--- Travelblog.Core/Interfaces/IBlogService.cs
+++ Travelblog.Core/Interfaces/IBlogService.cs
@@ -15,17 +15,10 @@ namespace Travelblog.Core.Interfaces
         public Blog LikeBlog(Blog blog, User user);
         public Blog UnLikeBlog(Blog blog, User user);
         public Blog AddPost (Post post);
-        public Blog MakePrivate(Blog blog);
-        public Blog MakePublic(Blog blog);
-        public Blog MakeSuspended(Blog blog);
-        public Blog RemoveSuspension(Blog blog);
-        public Blog DeleteBlog(Blog blog);
-        public Blog RestoreBlog(Blog blog);
         public Blog AddFollower(Blog blog, User user);
         public Blog RemoveFollower(Blog blog, User user);
         public List<User> GetFollowers(int Id);
         public Blog AddCountry(Country country);
         public Blog RemoveCountry(Country country);
-
     }
 }
